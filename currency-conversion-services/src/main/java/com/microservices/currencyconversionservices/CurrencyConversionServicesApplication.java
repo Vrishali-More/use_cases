108cb92af7256b611828ae34692379f84144b824
Vrishali-More <vrishali.more@kpipartners.com>
2022-10-27 15:29:06 +0530

    commit currency

--- currency-conversion-services/src/main/java/com/microservices/currencyconversionservices/CurrencyConversionServicesApplication.java
+++ currency-conversion-services/src/main/java/com/microservices/currencyconversionservices/CurrencyConversionServicesApplication.java
@@ -2,12 +2,9 @@ package com.microservices.currencyconversionservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("com.microservices.currencyconversionservice")
-@EnableDiscoveryClient
+
 public class CurrencyConversionServicesApplication {
 
 	public static void main(String[] args) {
